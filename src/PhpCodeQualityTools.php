7ab1a1628984f4f5c9db0784461f6f7d15f56e48
falvarez <falvarez@bitban.com>
2016-07-22 09:13:22 +0200

    Update tool version to 0.9.5

--- src/PhpCodeQualityTools.php
+++ src/PhpCodeQualityTools.php
@@ -10,5 +10,5 @@ namespace Bitban\PhpCodeQualityTools;
 class PhpCodeQualityTools
 {
     const APP_NAME = 'Bitban Technologies PHP Code Quality Tools';
-    const APP_VERSION = '0.9.4';
+    const APP_VERSION = '0.9.5';
 }
