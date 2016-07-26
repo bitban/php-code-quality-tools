c82010c1f0aa4f4ae41624b78c0228fcb8a30000
falvarez <falvarez@bitban.com>
2016-07-26 15:41:08 +0200

    Calculate version based on latest tag and show error message if tools are outdated

--- src/Console/Application.php
+++ src/Console/Application.php
@@ -13,16 +13,24 @@ use Bitban\PhpCodeQualityTools\Command\GitHooks\CheckCommand;
 use Bitban\PhpCodeQualityTools\Command\GitHooks\InstallCommand;
 use Bitban\PhpCodeQualityTools\Command\GitHooks\PostCheckoutCommand;
 use Bitban\PhpCodeQualityTools\Command\GitHooks\PostMergeCommand;
-use Bitban\PhpCodeQualityTools\PhpCodeQualityTools;
 use Symfony\Component\Console\Application as BaseApplication;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
 
 class Application extends BaseApplication
 {
+    const APP_NAME = 'Bitban Technologies PHP Code Quality Tools';
+
+    private $localVersion;
+    private $remoteVersion;
+
     public function __construct()
     {
-        parent::__construct(PhpCodeQualityTools::APP_NAME, PhpCodeQualityTools::APP_VERSION);
+        $this->remoteVersion = exec('git describe `git remote` --abbrev=0 --tags');
+        $this->localVersion = exec('git describe --abbrev=0 --tags');
 
-        // project:create
+        parent::__construct(self::APP_NAME, trim($this->localVersion, 'v'));
+
         $this->addCommands([
             new CheckCommand(),
             new InstallCommand(),
@@ -32,5 +40,19 @@ class Application extends BaseApplication
             new FixCodeStyleCommand(),
             new ValidateCommand()
         ]);
+    }
+
+    private function checkVersion(OutputInterface $output)
+    {
+        $versionDiff = version_compare($this->remoteVersion, $this->localVersion);
+        if ($versionDiff > 0) {
+            $output->writeln("\n<error>You don't have the latest version of Php Code Quality Tools. Please update to $this->remoteVersion.</error>\n");
+        }
+    }
+
+    public function doRun(InputInterface $input, OutputInterface $output)
+    {
+        $this->checkVersion($output);
+        return parent::doRun($input, $output);
     }
 }
